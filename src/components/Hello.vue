62a7070d588bf4d64516f51ee284b550f2adabd7
Carsten Mielke <carsten.mielke@kartverket.no>
2017-02-12 21:18:54 +0100

    update main

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2><a href="#/search">Search route</a></h2>
     <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
