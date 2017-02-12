d8aef8e05a116b8ec1c4b1f7810021c3dbfdf879
Carsten Mielke <carsten.mielke@kartverket.no>
2017-02-12 21:08:56 +0100

    add search route

---
+++ src/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="search" class="search-bar-overlay">
+    <input v-model="message">
+    <p>{{ message }}</p>
+  </div>
+</template>
+<script>
+  import * as searchServices from '../services/search';
+
+  export default {
+    name: 'Search',
+    data() {
+      return {
+        message: 'Test!',
+      };
+    },
+    mounted() {
+      console.log(searchServices);
+    },
+  };
+
+</script>
+<style>
+  #search {
+    text-align: left;
+    color: #2c3e50;
+  }
+
+</style>
